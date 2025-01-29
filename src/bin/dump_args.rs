e2571be6bfe17a136bb03adb793349b9edfc79ec
Gabriel Ferrer <ferrer@hendrix.edu>
2025-01-29 12:03:08 -0600

    Class Wednesday 1/29

--- src/bin/dump_args.rs
+++ src/bin/dump_args.rs
@@ -1,10 +1,15 @@
 use std::env;
-use std::fs::write;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = args[1].as_str();
-    for i in 2..args.len() {
-        write(filename, args[i].as_str()).unwrap();
+    let args: Vec<String> = env::args().skip(1).collect();
+    let filename = args[0].as_str();
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(filename).unwrap();
+    for i in 1..args.len() {
+        writeln!(file, "{}", args[i].as_str()).unwrap();
     }
 }
---
+++ src/bin/file_examine.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::fs::read_to_string;
+
+fn main() {
+    let mut args = env::args();
+    let my_name = args.next().unwrap();
+    for arg in args {
+        match print_line_nums(arg.as_str()) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("{my_name}: {arg}: {e}");
+            }
+        }
+    }
+}
+
+fn print_line_nums(filename: &str) -> std::io::Result<()> {
+    let file_str = read_to_string(filename)?;
+    println!("Contents of {filename}:");
+    for (line_num, line) in file_str.lines().enumerate() {
+        println!("{}: {line}", line_num + 1);
+    }
+    Ok(())
+}
