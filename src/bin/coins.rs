4ce295661bac7c32518b293bda4410fa8d06843e
Gabriel Ferrer <ferrer@hendrix.edu>
2025-02-28 12:11:45 -0600

    Friday 2/28

--- src/bin/coins.rs
+++ src/bin/coins.rs
@@ -1,21 +1,41 @@
-use std::{sync::Arc, thread};
+use std::{sync::{Arc, Mutex}, thread};
 
 fn main() {
     let num_simulations = 8;
     let target_flips = 10;
-    let mut num_completed = Arc::new(0);
+    let num_completed = Arc::new(Mutex::new(0));
+    let min_iterations = Arc::new(Mutex::new(u64::max_value()));
 
     for _ in 0..num_simulations {
+        let num_completed = num_completed.clone();
+        let min_iterations = min_iterations.clone();
         thread::spawn(move || {
-            simulate(target_flips);
-            num_completed += 1;
+            let iterations = simulate(target_flips);
+            println!("iterations: {iterations}");
+            {
+                let mut min_iterations = min_iterations.lock().unwrap();
+                if *min_iterations > iterations {
+                    *min_iterations = iterations;
+                }
+            }
+
+            let mut num_completed = num_completed.lock().unwrap();
+            *num_completed += 1;
         });
     }
 
-    while num_completed < num_simulations {}
+    loop {
+        let num_completed = num_completed.lock().unwrap();
+        if *num_completed == num_simulations {
+            break;
+        }
+    }
+
+    let min_iterations = min_iterations.lock().unwrap();
+    println!("min iterations: {}", *min_iterations);
 }
 
-fn simulate(target_flips: u64) {
+fn simulate(target_flips: u64) -> u64 {
     let mut consecutive = 0;
     let mut iterations = 0;
     while consecutive < target_flips {
@@ -26,5 +46,5 @@ fn simulate(target_flips: u64) {
             consecutive = 0;
         }
     }
-    println!("iterations: {iterations}");
+    iterations
 }
